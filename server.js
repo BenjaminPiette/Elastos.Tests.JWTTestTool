3dad6e21db285b524fc7bb4ba9b718aba5dbda1c
Benjamin Piette <benjamin.piette@elastos-fusion.org>
2019-12-13 16:19:10 +0800

    Minor improvements by Dongxiao

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const os = require("os")
 var jwt = require('jsonwebtoken')
+var bodyParser = require("body-parser");
 
 function getAllLocalIPAddresses() {
     var allUsableInterfaces = [];
@@ -12,7 +13,7 @@ function getAllLocalIPAddresses() {
             // Skip over non-ipv4 addresses and localhost
             if (iface.family !== 'IPv4' || iface.internal)
                 return;
-            
+
             allUsableInterfaces.push(iface);
         });
     });
@@ -20,11 +21,7 @@ function getAllLocalIPAddresses() {
     return allUsableInterfaces.map(addr => addr.address);
 }
 
-app.get('/credaccessresponse/:JWT', function (req, res) {
-    console.log("Received response scheme request.");
-
-    let jwtToken = req.params.JWT;
-
+function printJWT(jwtToken) {
     console.log();
     console.log("Received JWT:");
     console.log(jwtToken);
@@ -33,24 +30,38 @@ app.get('/credaccessresponse/:JWT', function (req, res) {
     console.log("Decoded JWT:");
     let jwtPayload = jwt.decode(jwtToken);
     console.log(jwtPayload);
+    console.log("------ end --------");
+}
+
+app.get('/credaccessresponse/:JWT', function (req, res) {
+    console.log("Received response scheme request.");
+
+    console.log("Process redirecturl");
+    printJWT(req.params.JWT);
 
     res.json({
         decoded: jwtPayload
     });
 })
 
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.post("/credaccessresponse", function (req, res) {
+    console.log("post");
+    console.log("Process callbackurl");
+    printJWT(req.body.jwt);
+    res.send(" post successfully!");
+});
+
 let port = 3020;
 app.listen(port, '0.0.0.0', function () {
     console.log();
-    console.log('Server is running at http://0.0.0.0:'+port);
-    console.log();
-    console.log("Your IP addresses are:")
-    console.log(getAllLocalIPAddresses());
+    console.log('Server is running at ' + getAllLocalIPAddresses() + ':'+port);
     console.log();
 })
 
 /**
  * Call example:
- * 
+ *
  * http://localhost:3020/credaccessresponse/eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcHBpZCI6IjQ0MDY0NWUyMWFhYmQ2ODIwMTEwZjNkYzI0NDllMDJlMDVhMmU3ZGMyOWE1ZjJhNTg4MmM1MDlhNTY2ZmQ3ZjJhYTRkMGJkMmJjYTA0MGEyZjNhN2RjOGI4ZGMyZWMxNGE5ODY4OGIyODYwMmY2YTJlZmIzZjA1MTNlZjA0YWM0IiwiaXNzIjoiZGlkOmVsYXN0b3M6aVVRdG9Ib1F4OHpneFJjTHg2RnhMS0U0ZVlKaUV6OG56QyIsImlhdCI6MTU2NjM1MjIxMywiZXhwIjoxNTY2MzgyMjEzLCJjYWxsYmFja3VybCI6Imh0dHA6Ly9ZT1VSSVA6MzAyMC9jcmVkYWNjZXNzcmVzcG9uc2UiLCJjbGFpbXMiOlt7ImVtYWlsIjp0cnVlfSx7Im5hbWUiOnRydWV9XX0.GqD1UZC_mDkzksSt_4QU6ZTlJaG2U8PIqEQ0M9oMpW4
  */
