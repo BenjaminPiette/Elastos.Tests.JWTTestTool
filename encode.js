b06523b97c707217ad8a96489f36efa2451467e4
Benjamin Piette <benjamin.piette@elastos-fusion.org>
2019-12-31 16:04:19 +0800

    Fixed claims format

--- encode.js
+++ encode.js
@@ -1,16 +1,36 @@
 var jwt = require('jsonwebtoken');
+const os = require("os")
+
+function getAllLocalIPAddresses() {
+    var allUsableInterfaces = [];
+
+    var ifaces = os.networkInterfaces();
+    Object.keys(ifaces).forEach(function(ifname) {
+        ifaces[ifname].forEach(function(iface) {
+            // Skip over non-ipv4 addresses and localhost
+            if (iface.family !== 'IPv4' || iface.internal)
+                return;
+
+            allUsableInterfaces.push(iface);
+        });
+    });
+
+    return allUsableInterfaces.map(addr => addr.address);
+}
+let url = "http://" + getAllLocalIPAddresses()[0] + ":3020/credaccessresponse";
 
 let jwtClaims = {
-    "appid": "440645e21aabd6820110f3dc2449e02e05a2e7dc29a5f2a5882c509a566fd7f2aa4d0bd2bca040a2f3a7dc8b8dc2ec14a98688b28602f6a2efb3f0513ef04ac4",
+    "appid": "",
     "iss":"did:elastos:iUQtoHoQx8zgxRcLx6FxLKE4eYJiEz8nzC",
     "iat":1566352213,
-    "exp":1566382213,
-    "callbackurl":"http://YOURIP:3020/credaccessresponse",
-    //"redirecturl":"myapp://elastosschemeresponse",
-    "claims": [
-        {"email": true},
-        {"name": true}
-     ]
+    // "exp":1566382213,
+    "callbackurl":url,
+    // "redirecturl": url //"myapp://elastosschemeresponse",
+    "claims": {
+        "email": true,
+        "name": true
+    },
+    "anotherfield":"something"
 }
 
 console.log();
@@ -18,7 +38,7 @@ console.log("JWT PAYLOAD:");
 console.log(jwtClaims);
 console.log();
 
-var token = jwt.sign(jwtClaims, 'secret');
+var token = jwt.sign(jwtClaims, 'secret', {algorithm: 'HS256'});
 
 console.log();
 console.log("JWT TOKEN:");
